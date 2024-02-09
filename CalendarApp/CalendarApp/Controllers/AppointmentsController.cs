60c79a343127d29f8ee4d4f40314fe214583c257
Nick <78423234+TheNickHimself@users.noreply.github.com>
2024-02-09 17:51:39 +0100

    more

--- CalendarApp/CalendarApp/Controllers/AppointmentsController.cs
+++ CalendarApp/CalendarApp/Controllers/AppointmentsController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Domain.Interfaces;
 using CalendarApp.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
 
 namespace CalendarApp.Controllers
 {
@@ -36,16 +39,35 @@ namespace CalendarApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(CreateAppointmentViewModel viewModel)
+        public IActionResult Create(CreateAppointmentViewModel viewModel, [FromServices] IWebHostEnvironment webHostEnv)
         {
             if (ModelState.IsValid)
             {
+                string relpaht = "";
+                if (viewModel.ImageFile != null && viewModel.ImageFile.Length > 0)
+                {
+                    // Generate a unique filename
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(viewModel.ImageFile.FileName);
+
+                    // Combine the wwwroot/images path with the unique filename
+                    var filePath = Path.Combine(webHostEnv.WebRootPath, "images", fileName);
+
+                    // Save the file to wwwroot/images
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        viewModel.ImageFile.CopyTo(fileStream);
+                    }
+
+                    // Save the file path to the PicturePath property in the model
+                    relpaht = "/images/" + fileName;
+                }
+
                 var appointment = new Domain.Models.Appointment
                 {
                     Date = viewModel.Date,
                     Title = viewModel.Title,
                     Description = viewModel.Description,
-                    PicturePath = viewModel.PicturePath ?? "NoPicture" // Handle nullability as needed
+                    PicturePath = relpaht ?? "NoPicture" // Handle nullability as needed
                 };
 
                 _appointmentService.AddAppointment(appointment);
